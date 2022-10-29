060433d6bb0f0fd9c33d50dc993ed6e3a94863b1
leewbatic <lee@horkans.com>
2022-10-29 23:36:24 +0100

    Shape

--- WindowCreator.java
+++ WindowCreator.java
@@ -7,14 +7,14 @@ public class WindowCreator {
         ShapesManager shapesManager = new ShapesManager();
         shapesManager.setDisplayName(true);
         shapesManager.setDisplayBoundingBox(false);
-        shapesManager.addShape(new Circle(200,200,Color.blue,false, 100));
-        shapesManager.addShape(new Rectangle(51,51,Color.red, false, 50,100));
+        shapesManager.addShape(new Circle(400,400,Color.blue,false, 80));
+        shapesManager.addShape(new Rectangle(50,300,Color.red, false, 75,100));
         shapesManager.addShape(new Square(600,100,Color.yellow,true,80));
 
         //Create and configure our JFrame (window)
         CustomWindow customWindow = new CustomWindow(shapesManager);
         customWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        customWindow.setTitle("Test Window");
+        customWindow.setTitle("L00177784 CA1");
         customWindow.setVisible(true);
 
     }
